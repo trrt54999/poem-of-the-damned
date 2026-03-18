5548267ce215591f727069969b11309b5703c2d4
trrt54999 <moki22810sobakacom@gmail.com>
2026-03-18 23:28:29 +0200

    feat: implement next (continue scene) method in game state machine

--- src/main/java/com/midnightdraft/poemofthedamned/domain/engine/GameStateMachine.java
+++ src/main/java/com/midnightdraft/poemofthedamned/domain/engine/GameStateMachine.java
@@ -1,9 +1,12 @@
 package com.midnightdraft.poemofthedamned.domain.engine;
 
 import com.midnightdraft.poemofthedamned.domain.model.Dialogue;
+import com.midnightdraft.poemofthedamned.domain.model.GameCharacter;
+import com.midnightdraft.poemofthedamned.domain.model.GameCharacterSprite;
 import com.midnightdraft.poemofthedamned.domain.model.GameScene;
 import com.midnightdraft.poemofthedamned.domain.model.SaveFlag;
 import java.util.List;
+import java.util.Optional;
 
 public class GameStateMachine {
 
@@ -30,9 +33,25 @@ public class GameStateMachine {
     this.index = 0;
   }
 
-  // nazva gavno
-  public void next(){
+  public DialogueStep continueScene(){
+    Dialogue dialogue = dialogues.get(index++);
 
+    if(index >= dialogues.size()) {
+      currentState = GameState.WAITING_FOR_CHOICE;
+    }
+
+    if (dialogue.getMusicPath() != null) {
+      this.currentMusicPath = dialogue.getMusicPath();
+    }
+
+    return new DialogueStep(
+        Optional.ofNullable(dialogue.getGameCharacter())
+            .map(GameCharacter::getName),
+        Optional.ofNullable(dialogue.getGameCharacterSprite())
+            .map(GameCharacterSprite::getSpritePath),
+        Optional.ofNullable(dialogue.getMusicPath()),
+        Optional.ofNullable(dialogue.getSpritePosition()),
+        dialogue.getText(),
+        currentScene.getBackgroundPath());
   }
 }
-
